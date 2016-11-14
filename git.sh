4e2b4868f6d15d0b1452fc8bbc5752a748d9bca1
Tanner Grehawick <tgrehawi@gmail.com>
2016-11-14 12:50:03 -0500

    init commit
    + bootstrap script
    + git aliases

---
+++ git.sh
@@ -0,0 +1,6 @@
+
+# shell git aliases
+alias gs='git status'
+
+# git aliases
+git config --global alias.grog "log --pretty=format:\"%s%n%C(black bold)commit %C(dim green)%h%Creset %C(black bold) by %C(white dim)%an%C(auto)%d\" --graph"
