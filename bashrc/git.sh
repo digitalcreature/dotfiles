e18a0d144f750fcb4bffabdef5faac494bc6bcc4
Tanner Grehawick <tgrehawi@gmail.com>
2017-01-07 19:23:49 -0500

    add git alias to check if in work dir: git checkrepo

--- bashrc/git.sh
+++ bashrc/git.sh
@@ -4,3 +4,4 @@ alias gs='git status'
 
 # git aliases
 git config --global alias.grog "log --pretty=format:\"%s%n%C(black bold)commit %C(dim green)%h%Creset %C(black bold) by %C(white dim)%an%C(auto)%d\" --graph"
+git config --global alias.checkrepo '!bash -c "git rev-parse --is-inside-work-tree &> /dev/null"'
