389a69c2eefa0e2a6b8c378e84509f8be642a16f
Tanner Grehawick <tgrehawi@gmail.com>
2017-01-23 15:53:29 -0500

    add ao

--- bashrc/bash.sh
+++ bashrc/bash.sh
@@ -1,2 +1,9 @@
 
 # general utility stuff
+
+# ao: 'atom open'
+# usage: <command> | ao
+# opens all files listed by output of <command> in atom
+# 	(very useful when using template.sh; `template lua/class Foo | ao`
+# 	will create a new file from the 'lua.class' template and open it)
+alias ao="xargs atom"
