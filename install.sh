813fdc77490195d313c5cba57eb77724ae115649
Tanner Grehawick <tgrehawi@gmail.com>
2017-04-16 23:10:13 -0400

    unify .bashrc and powerline into single install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Installing powerline..."
+cd powerline-shell
+install.py
+cd ..
+if [ -f ~/.bashrc ]; then
+	echo "Backing up old .bashrc..."
+	rm -f ~/.bashrc.bak
+	mv ~/.bashrc ~/.bashrc.bak
+fi
+echo "Copying .bashrc..."
+cp .bashrc ~/.bashrc
+echo "export DOTFILES_HOME=\""$PWD"\"" >> ~/.bashrc
+echo "source $DOTFILES_HOME/bootstrap" >> ~/.bashrc
+echo "Installation complete!"
